1a138cd663ebf799f1fe24de0144fc3e0658cdea
xiaofei <zhongda@parasynn.com>
2022-01-17 16:00:06 +0800

    feat: Access-Control-Allow-Headers

--- pkg/httpcode/middleware.go
+++ pkg/httpcode/middleware.go
@@ -8,6 +8,9 @@ import (
 // HeaderMiddleware 设置请求头
 func HeaderMiddleware(ctx iris.Context) {
 	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Authorization")
+	// Access-Control-Allow-Headers 最好明确规定, 部分浏览器不兼容通配符, 参阅下文
+	// https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
 	ctx.Header("Access-Control-Allow-Headers", "*")
 	ctx.Header("content-type", "application/json;charset=utf-8")
 	if ctx.Request().Method == "OPTIONS" {
