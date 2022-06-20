cc5159df8141f6bab12b439b2222c281d9e1c54f
xiaofei <17610635681@163.com>
2022-06-20 14:31:37 +0800

    feat: recover print stack

--- pkg/safego/safe/index.go
+++ pkg/safego/safe/index.go
@@ -3,7 +3,7 @@ package safe
 import (
 	"fmt"
 	"log"
-	"runtime"
+	"runtime/debug"
 )
 
 // Go 安全go程.
@@ -11,8 +11,8 @@ func Go(f func()) {
 	go func() {
 		defer func() {
 			if err := recover(); err != nil {
-				_, file, line, _ := runtime.Caller(3)
-				log.Println(fmt.Sprintf("recover err: %v, line: %s", err, fmt.Sprintf("%s:%d", file, line)))
+				log.Println(fmt.Sprintf("recover err: %v", err))
+				debug.PrintStack()
 			}
 		}()
 		f()
@@ -24,8 +24,8 @@ func GoWithField(f func(val interface{}), val interface{}) {
 	go func(val interface{}) {
 		defer func() {
 			if err := recover(); err != nil {
-				_, file, line, _ := runtime.Caller(3)
-				log.Println(fmt.Sprintf("recover err: %v, line: %s", err, fmt.Sprintf("%s:%d", file, line)))
+				log.Println(fmt.Sprintf("recover err: %v", err))
+				debug.PrintStack()
 			}
 		}()
 		f(val)
